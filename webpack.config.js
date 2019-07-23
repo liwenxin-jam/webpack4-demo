43ad4ff5d6f267e592f37f4b9c48d75372496b9a
liwenxin-jam <mxlwx@126.com>
2019-07-23 11:28:15 +0800

    add noparse

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    noParse: /jquery|lodash/, // 不去解析jquery中的依赖，相对提高打包速度
+    rules: [{
+      test: /\.js$/, // normal 普通的loader
+      use: {
+        loader: 'babel-loader', // 用babel-loader 需要把es6转成es5
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react'],
+        }
+      }
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
+}
