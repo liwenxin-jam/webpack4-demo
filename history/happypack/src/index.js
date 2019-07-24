cd05cdbb21dd0be1e2ee24c2d16158bce7f4409d
liwenxin-jam <mxlwx@126.com>
2019-07-24 11:06:57 +0800

    add happypack

---
+++ history/happypack/src/index.js
@@ -0,0 +1,16 @@
+// import jquery from 'jquery'
+// import moment from 'moment'
+
+// // 设置语言，IgnorePlugin忽略需要手动引入
+// import 'moment/locale/zh-cn';
+// moment.locale('zh-cn');
+
+// let r = moment().endOf('day').fromNow();
+// console.log(r);
+
+import React from 'react';
+import { render } from 'react-dom';
+
+render(<h1>jsx</h1>, window.root);
+
+
--- history/happypack/webpack.config.js
+++ history/happypack/webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const Webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+// 模块 happypack 可以实现多线程来打包 资源小没有必要，反而会增加打包时间
+const Happypack = require('happypack');
 
 module.exports = {
   mode: 'development',
@@ -20,14 +22,32 @@ module.exports = {
           presets: ['@babel/preset-env', '@babel/preset-react'],
         }
       },
+      // use: 'Happypack/loader?id=js', // 多线程loader，指定id
       include: path.resolve(__dirname, 'src'), // 指定查找某个文件夹
       exclude: /node_modules/ // 排除某个文件夹
     }, {
       test: /\.css$/,
       use: ['style-loader', 'css-loader']
+      // use: 'Happypack/loader?id=css', // 多线程loader，指定id
     }]
   },
   plugins: [
+    // new Happypack({
+    //   id: 'js',
+    //   use: [{
+    //     loader: 'babel-loader', // 用babel-loader 需要把es6转成es5
+    //     options: {
+    //       presets: ['@babel/preset-env', '@babel/preset-react'],
+    //     }
+    //   }]
+    // }),
+    // new Happypack({
+    //   id: 'css',
+    //   use: ['style-loader', 'css-loader']
+    // }),
+    // new Webpack.DllReferencePlugin({
+    //   manifest: path.resolve(__dirname, 'dist', 'manifest.json')
+    // }),
     new Webpack.IgnorePlugin(/\.\/locale/, /moment/), // 引入moment忽略加载locale文件
     new HtmlWebpackPlugin({
       template: './public/index.html'
